0db0af7e3352aaedc350a40ed72ca086b2c3f589
SonamNarula <sonamnarula2108@gmail.com>
2026-01-12 19:11:57 +0530

    Initial commit – added C++ basics

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " enter radius of the circle : " << endl;
+    float radii;
+    cin >> radii;
+
+    float pi = 3.1415;
+
+    cout << "area of circle = " << " "
+         << pi * radii * radii;
+
+    return 0;
+}
