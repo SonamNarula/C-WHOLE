255335e2c52deed161a6d279a9fcae7a8273557f
SonamNarula <sonamnarula2108@gmail.com>
2026-01-16 10:15:29 +0530

    Add POINTERS folder

---
+++ POINTERS/DereferenceOperator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 12;
+    int* ptr = &x;
+    cout << x << endl;
+    cout << *ptr << endl;
+    cout << &ptr << endl;
+
+    *ptr = 21;
+    cout<<x<<endl;
+    return 0;
+}
